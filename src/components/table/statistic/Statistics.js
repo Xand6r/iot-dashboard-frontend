ed393c55f3447950ac8e6c9e340b451b27471ac1
xander <shuaibualexander@gmail.com>
2021-10-10 00:22:58 +0100

    add changes

--- src/components/table/statistic/Statistics.js
+++ src/components/table/statistic/Statistics.js
@@ -1,87 +1,90 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-// import Chart from './chart';
-import { Statistic, Card, Row, Col } from 'antd';
-import { getReq } from "../../../api";
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import './Statistic.scss'
+import Chart from "./chart";
+import { postReq } from "../../../api";
+import { DatePicker, Space, Spin } from "antd";
 
-export default function Statistics(){
-  const [state, setState] = useState({
-    pMax: 0,
-    fMax: 0,
-    pMeam: 0,
-    fMeam: 0
-  });
+
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import "./Statistic.scss";
+
+const { RangePicker } = DatePicker;
+const DEFAULT_PAGINATION = {
+  page: 1,
+  limit: 10,
+};
+
+export default function Statistics() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { labid } = useParams();
-  useEffect(() => {
-    getReq('/stats/1')
-    .then(({data: res}) => {
-      const {
-        pMax,
-        fMax,
-        pMeam,
-        fMeam,
-      } = res;
-      setState({
-        pMax,
-        fMax,
-        pMeam,
-        fMeam
-      });
-    })
-  },[]);
 
-return(
-  <div className="site-statistic-demo-card">
-    {/* <Chart /> */}
-    {/* <Row className="rows" gutter={16}>
-      <Col span={5}>
-        <Card className="statistic-demo-card">
-          <Statistic
-            title="Average Flow Rate"
-            value={state.fMeam}
-            precision={2}
-            valueStyle={{ color: '#3f8600' }}
-            suffix="m3/s"
-          />
-        </Card>
-      </Col>
-      <Col span={5}>
-      <Card className="statistic-demo-card">
-          <Statistic
-            title="Average Pressure"
-            value={state.pMeam}
-            precision={2}
-            valueStyle={{ color: '#3f8600' }}
-            suffix="Pa"
-          />
-        </Card>
-      </Col>
-      <Col span={5}>
-      <Card className="statistic-demo-card">
-          <Statistic
-            title="Maximum Flow Rate"
-            value={state.fMax}
-            precision={2}
-            valueStyle={{ color: '#3f8600' }}
-            suffix="m3/s"
-          />
-        </Card>
-      </Col>
-      <Col span={5}>
-      <Card className="statistic-demo-card">
-          <Statistic
-            title="Maximum Pressure"
-            value={state.pMax}
-            precision={2}
-            valueStyle={{ color: '#3f8600' }}
-            suffix="Pa"
-          />
-        </Card>
-      </Col>
-    </Row>
-   */}
-  </div>
-);
+  useEffect(() => {
+    postReq("/filter", {
+      page: DEFAULT_PAGINATION.page,
+      limit: DEFAULT_PAGINATION.limit,
+    })
+      .then(({ data: res }) => {
+        setData(res.docs);
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  const onDateChange = (date) => {
+    const [start, stop] = date;
+    // fetch new data based on the pagination data
+    const filter = {
+      date: {
+        $gte: start,
+        $lte: stop
+      },
+    };
+    setLoading(true);
+    postReq("/filter", {
+      page: DEFAULT_PAGINATION.page,
+      limit: DEFAULT_PAGINATION.limit,
+      filter
+    })
+      .then(({ data: res }) => {
+        setData(res.docs);
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return (
+    <div className="site-statistic-demo-card">
+      {
+        !data.length && !loading ? (
+          <Spin spinning={loading} tip="Loading...">
+            <div className="range-wrapper">
+              <RangePicker onChange={onDateChange} />
+            </div>
+            <div className="chart-wrapper">
+              <Chart data={data} dataKey={"Pressure"} />
+            </div>
+
+            <div className="chart-wrapper">
+              <Chart data={data} dataKey={"flow"} />
+            </div>
+          </Spin>
+        ) : (
+          <h2>
+            No data to display for this range
+          </h2>
+        )
+      }
+    </div>
+  );
 }
--- src/components/table/statistic/chart.js
+++ src/components/table/statistic/chart.js
@@ -2,105 +2,85 @@ import React, { useState } from "react";
 import moment from "moment";
 import ReactApexChart from "react-apexcharts";
 
-class ApexChart extends React.Component {
-  constructor(props) {
-    super(props);
+const SENSOR_COUNT = 3; //the number of sensors there are;
 
-    this.state = {
-      animations: {
+function ApexChart({ data, dataKey }) {
+  const state = {
+    theme: {
+      palette: "palette10", // upto palette10
+    },
+    animations: {
+      enabled: true,
+      easing: "easeinout",
+      speed: 800,
+      animateGradually: {
         enabled: true,
-        easing: "easeinout",
-        speed: 800,
-        animateGradually: {
+        delay: 150,
+      },
+      dynamicAnimation: {
+        enabled: true,
+        speed: 350,
+      },
+    },
+    series: [...Array(SENSOR_COUNT).keys()].map((index) => ({
+      name: `Pressure Sensor ${index + 1}`,
+      data: data.map((data) => data[dataKey.toLowerCase()][index]).flat(),
+    })),
+    options: {
+      chart: {
+        height: 700,
+        type: "line",
+        dropShadow: {
           enabled: true,
-          delay: 150,
-        },
-        dynamicAnimation: {
-          enabled: true,
-          speed: 350,
+          color: "#000",
+          top: 18,
+          left: 7,
+          blur: 10,
+          opacity: 0.2,
         },
       },
-
-      series: [
-        {
-          name: "High - 2013",
-          data: [28, 29, 12, 45, 67, 11, 43],
-          labels: [
-            "01 Jan 2001 16:20",
-            "02 Jan 2001 16:20",
-            "03 Jan 2001 16:20",
-            "04 Jan 2001 16:20",
-            "05 Jan 2001 16:20",
-            "06 Jan 2001 16:20",
-            "07 Jan 2001 16:29",
-          ],
+      dataLabels: {
+        enabled: true,
+      },
+      stroke: {
+        curve: "smooth",
+      },
+      title: {
+        text: `A graph of ${dataKey} vs Time`,
+        align: "left",
+      },
+      grid: {
+        borderColor: "#e7e7e7",
+        row: {
+          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5,
         },
-        {
-          name: "Low - 2013",
-          data: [12, 11, 34, 23, 34, 45, 55],
-        },
-      ],
-      options: {
-        chart: {
-          height: 700,
-          type: "line",
-          dropShadow: {
-            enabled: true,
-            color: "#000",
-            top: 18,
-            left: 7,
-            blur: 10,
-            opacity: 0.2,
-          },
-        },
-        colors: ["#77B6EA", "#545454"],
-        dataLabels: {
-          enabled: true,
-        },
-        stroke: {
-          curve: "smooth",
-        },
+      },
+      xaxis: {
         title: {
-          text: "Average High & Low Temperature",
-          align: "left",
+          text: "Date",
         },
-        grid: {
-          borderColor: "#e7e7e7",
-          row: {
-            colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-            opacity: 0.5,
-          },
-        },
-        markers: {
-          size: 1,
-        },
-        xaxis: {
-          title: {
-            text: "Month",
-          },
-          type: "datetime",
-        },
-        yaxis: {
-          title: {
-            text: "Temperature",
-          },
-        }
+        type: "datetime",
       },
-    };
-  }
+      labels: data.map(({ date }) => date),
+      yaxis: {
+        title: {
+          text: `${dataKey}`,
+        },
+      },
+    },
+  };
 
-  render() {
-    return (
-      <div id="chart">
-        <ReactApexChart
-          options={this.state.options}
-          series={this.state.series}
-          type="line"
-          height={350}
-        />
-      </div>
-    );
-  }
+  return (
+    <div id="chart">
+      <ReactApexChart
+        options={state.options}
+        series={state.series}
+        type="line"
+        height={350}
+      />
+    </div>
+  );
 }
 
 export default ApexChart;
